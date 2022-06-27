658fa44a5169cdfdb4f9d2aa69cd760a3a5226f4
Mawadatul Maulidah <106209646+mawadatulmaulidah@users.noreply.github.com>
2022-06-27 13:47:29 +0700

    Fix done challenge-09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,22 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+    const {data} = props;
+
     return (
-        <></>
+        <div className="card center">
+            <div className='cardFoto'>
+                <img src={data.photo}></img>
+            </div>
+            <div className="cardDetail">
+                <h4>{data.name}</h4>
+                <br></br>
+                <p>{data.phone}</p>
+                <br></br>
+                <p>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
